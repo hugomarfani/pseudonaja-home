7c2ac742c2ffed87a976a2b216feb868c73420e9
Hugo Marfani <58790608+hugomarf@users.noreply.github.com>
2022-07-20 09:33:43 +0100

    Two backgrounds

--- assets/dist/scripts.js
+++ assets/dist/scripts.js
@@ -12904,51 +12904,76 @@ return jQuery;
 })));
 //# sourceMappingURL=popper.js.map
 
-$('document').ready(function(){
-    // Animations
-    let lotties = [
+$("document").ready(function () {
+  // Animations
+  let lotties = [
     (line1Anim = lottie.loadAnimation({
-        container: document.querySelector(".bg-img"),
+      container: document.querySelector("#bg-img1"),
+      renderer: "svg",
+      loop: false,
+      autoplay: false,
+      path: window.location.origin + "/assets/lottie/bg-1.json",
+    })),
+    (line2Anim = lottie.loadAnimation({
+        container: document.querySelector("#bg-img2"),
         renderer: "svg",
         loop: false,
         autoplay: false,
-        path: window.location.origin+"/assets/lottie/bg-1.json"
-    }))]
-    // Adds frame parameter to enable scrubbing with scrollTrigger
-    lotties.forEach(l => l.frame = 0);
-    let lottieLoaded = 0;
-    // Checks if each lottie loaded
-    lotties.forEach(function(e) {
-        e.addEventListener("DOMLoaded", function() {
-            lottieLoaded++
-            isComplete()
-        });
+        path: window.location.origin + "/assets/lottie/bg-2.json",
+      })),
+  ];
+  // Adds frame parameter to enable scrubbing with scrollTrigger
+  lotties.forEach((l) => (l.frame = 0));
+  let lottieLoaded = 0;
+  // Checks if each lottie loaded
+  lotties.forEach(function (e) {
+    e.addEventListener("DOMLoaded", function () {
+      lottieLoaded++;
+      isComplete();
     });
-    // Runs only after all lotties loaded
-    function isComplete() {
-        if (lottieLoaded >= lotties.length) {
-            tlReady()
-        }
+  });
+  // Runs only after all lotties loaded
+  function isComplete() {
+    if (lottieLoaded >= lotties.length) {
+      tlReady();
     }
-    function tlReady() {
-        let line1Tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: ".bg-img",
-                start: "top center",
-                end: "center center",
-                markers: false,
-                pin: false,
-                invalidateOnRefresh: true,
-                scrub: true
-            }
-        });
-        line1Tl
-        .to(line1Anim, {
-            frame: line1Anim.totalFrames - 1,
-            ease: "none",
-            duration: 2,
-            onUpdate: () => line1Anim.goToAndStop(line1Anim.frame, true),
-        });
-    }
+  }
+  function tlReady() {
+    let line1Tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#bg-img1",
+        start: "top center",
+        end: "center center",
+        markers: false,
+        pin: false,
+        invalidateOnRefresh: true,
+        scrub: true,
+      },
+    });
+    line1Tl.to(line1Anim, {
+      frame: line1Anim.totalFrames - 1,
+      ease: "none",
+      duration: 2,
+      onUpdate: () => line1Anim.goToAndStop(line1Anim.frame, true),
+    });
+    let line2Tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: "#bg-img2",
+          start: "top bottom",
+          end: "center center",
+          markers: false,
+          pin: false,
+          invalidateOnRefresh: true,
+          scrub: true,
+        },
+      });
+      line2Tl.to(line2Anim, {
+        frame: line1Anim.totalFrames - 1,
+        ease: "none",
+        duration: 2,
+        onUpdate: () => line2Anim.goToAndStop(line2Anim.frame, true),
+      })
+  }
 });
+
 //# sourceMappingURL=scripts.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,93 @@
 // Importing gulp API functions
 const { src, dest, watch, series, parallel } = require("gulp");
-const sourcemaps = require('gulp-sourcemaps');
+const sourcemaps = require("gulp-sourcemaps");
 // CSS related packages
-const sass = require('gulp-sass')(require('node-sass'));
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const sass = require("gulp-sass")(require("node-sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
 // JS related packages
-const jquery = require('jquery');
-const popper = require('@popperjs/core');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const jquery = require("jquery");
+const popper = require("@popperjs/core");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
 // Path to files
 const files = {
-    fileDest: 'assets/dist/',
-    scssPath: 'assets/css/main.scss',
-    jsPath: [
-        'node_modules/jquery/dist/jquery.js',
-        'node_modules/gsap/dist/gsap.min.js',
-        'node_modules/lottie-web/build/player/lottie.min.js',
-        'node_modules/gsap/dist/ScrollTrigger.min.js',
-        'node_modules/@popperjs/core/dist/umd/popper.js',
-        'assets/js/main.js'
-    ],
-    cssWatch: 'assets/css/**/*.scss',
-    jsWatch: 'assets/js/**/*.js'
-}
+  fileDest: "assets/dist/",
+  scssPath: "assets/css/main.scss",
+  jsPath: [
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/gsap/dist/gsap.min.js",
+    "node_modules/lottie-web/build/player/lottie.min.js",
+    "node_modules/gsap/dist/ScrollTrigger.min.js",
+    "node_modules/@popperjs/core/dist/umd/popper.js",
+    "assets/js/main.js",
+  ],
+  cssWatch: "assets/css/**/*.scss",
+  jsWatch: "assets/js/**/*.js",
+};
 // CSS: Creates sourcemap, compiles sass, lints and minifies it
 function cssTask(cb) {
-    return src(files.scssPath)
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([ autoprefixer()]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest(files.fileDest));
-    cb();
+  return src(files.scssPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(files.fileDest));
+  cb();
 }
 function cssTaskFull(cb) {
-    return src(files.scssPath)
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([ autoprefixer(), cssnano()]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest(files.fileDest));
-    cb();
+  return src(files.scssPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(files.fileDest));
+  cb();
 }
 // Combines all JS files and minifies it
 function jsTask(cb) {
-    return src(files.jsPath)
-        .pipe(sourcemaps.init())
-        .pipe(concat('scripts.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest(files.fileDest))
-    cb();
+  return src(files.jsPath)
+    .pipe(sourcemaps.init())
+    .pipe(concat("scripts.js"))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(files.fileDest));
+  cb();
 }
 function jsTaskFull(cb) {
-    return src(files.jsPath)
-        .pipe(sourcemaps.init())
-        .pipe(concat('scripts.js'))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest(files.fileDest))
-    cb();
+  return src(files.jsPath)
+    .pipe(sourcemaps.init())
+    .pipe(concat("scripts.js"))
+    .pipe(uglify())
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(files.fileDest));
+  cb();
 }
 // Watch Tasks
-function watchTask(){
-    watch(
-        [files.cssWatch, files.jsWatch],
-        {interval: 1000, usePolling: true},
-        series(
-            parallel(cssTask, jsTask)
-        )
-    );
+function watchTask() {
+  watch(
+    [files.cssWatch, files.jsWatch],
+    { interval: 1000, usePolling: true },
+    series(parallel(cssTask, jsTask))
+  );
 }
-function watchCss(){
-    watch(
-        files.cssWatch,
-        {interval: 1000, usePolling: true},
-        series(
-            parallel(cssTask)
-        )
-    );
+function watchCss() {
+  watch(
+    files.cssWatch,
+    { interval: 1000, usePolling: true },
+    series(parallel(cssTask))
+  );
 }
-function watchJs(){
-    watch(
-        files.jsPath,
-        {interval: 1000, usePolling: true},
-        series(
-            parallel(jsTask)
-        )
-    );
+function watchJs() {
+  watch(
+    files.jsPath,
+    { interval: 1000, usePolling: true },
+    series(parallel(jsTask))
+  );
 }
-function buildTaskFull(cb){
-    series(
-        parallel(cssTaskFull, jsTaskFull)
-    );
-    cb();
+function buildTaskFull(cb) {
+  series(parallel(cssTaskFull, jsTaskFull));
+  cb();
 }
 exports.css = cssTask;
 exports.cssFull = cssTaskFull;
